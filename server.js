4730380d73ca6e86fb7938b9653a37af1b9f2037
Jose Contreras <jocontre@joselcontreras.com>
2014-08-14 00:37:54 -0700

    Adding and testing routes

--- server.js
+++ server.js
@@ -7,15 +7,31 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 4001;
 
-var router = express.Router();
-
 var mongoose = require('mongoose');
-mongoose.connect(' mongodb://hosay:Snoogan9s@ds055699.mongolab.com:55699/bearz');
+mongoose.connect('mongodb://hosay:Snoogan9s@ds055699.mongolab.com:55699/bearz');
 
 var Bear = require('./app/models/bear'); // the bear schema
 
-router.get('/', function (req, res) {
+
+// ROUTES
+var router = express.Router();
+
+router.use(function(req, res, next){
+	console.log('testing route for happenings!');
+	next(); //go to next route.
+});
+
+router.get('/', function (req, res) { // works at localhost:4001/api
 	res.json({message: 'Aww yeah, working as expected.'});
+});
+
+router.route('/bears').post(function(req, res){
+	var bear = new Bear();
+	bear.name = req.body.name; // Set name from request.
+
+	bear.save(function(err){
+		err ? res.send(err) : res.json({message: 'Bear created'}); // I like to use ternary when possible
+	});
 });
 
 app.use('/api', router); // all routes will be prefixed with api
